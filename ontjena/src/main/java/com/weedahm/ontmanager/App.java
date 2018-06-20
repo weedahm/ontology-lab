9e9e8e9a6f5a7a1587af61790726d2d4de964c76
banana <2323hsb@gmail.com>
2018-06-20 14:19:58 +0900

    re commit

---
+++ ontjena/src/main/java/com/weedahm/ontmanager/App.java
@@ -0,0 +1,13 @@
+package com.weedahm.ontmanager;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
